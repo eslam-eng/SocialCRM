ef09600016827591d6f44e27db7da775757d4259
eslam <elbadry693@gmail.com>
2025-07-23 00:02:54 +0300

    refactor: remove unused `features` relationship method from `PlanSubscription` model

--- app/Models/PlanSubscription.php
+++ app/Models/PlanSubscription.php
@@ -74,12 +74,6 @@ class PlanSubscription extends BaseModel
         };
     }
 
-    public function features()
-    {
-        return $this->belongsToMany(Feature::class, 'subscription_plan_features')
-            ->withPivot('value')->using(SubscriptionPlanFeature::class);
-    }
-
     public function activate(): void
     {
         $this->getState()->activate();
